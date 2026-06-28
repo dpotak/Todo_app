f8703ee8a33ba76304113d7f84c5c6284956abb8
dpotak <darjapotak248@gmail.com>
2026-06-28 16:15:33 +0300

    Remove files

--- Todo_app_vue/src/App.vue
+++ Todo_app_vue/src/App.vue
@@ -1,18 +1,49 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-   
-  </header>
+  <div class="app">
+    <h1>Todo App</h1>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <input v-model="task" @keyup.enter="addTask" placeholder="Add task" />
+
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.text }}
+        <button @click="removeTodo(todo.id)">x</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style>
+<script setup>
+import { ref } from 'vue'
 
+const task = ref('')
+const todos = ref([])
+
+function addTask() {
+  if (!task.value.trim()) return
+
+  todos.value.push({
+    id: Date.now(),
+    text: task.value
+  })
+
+  task.value = ''
+}
+
+function removeTodo(id) {
+  todos.value = todos.value.filter(t => t.id !== id)
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: Arial;
+  background: #050505;
+  color: white;
+}
+
+.app {
+  padding: 20px;
+}
 </style>
