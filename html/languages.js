4a875e3d2b7ef35a3efd6a71d718ef3523896971
dpotak <darjapotak248@gmail.com>
2026-06-28 14:56:51 +0300

    Add new changes

--- html/languages.js
+++ html/languages.js
@@ -5,11 +5,12 @@ en:{
     all_1: "All",
     active_1: "Active",
     tasks_here_yet: "No tasks here yet",
-    items_left: " items left",
+    items_left: "0 items left",
     clear_completed: "Clear completed",
     Russia_1: "Russia",
     English_1: "English",
     Estonian_1: "Estonian",
+    What_do_you_need: "What do you need to do?",
 
 },
 
@@ -19,12 +20,13 @@ ru:{
     complete_1: "Завершенный",
     all_1: "Все",
     active_1: "Активный",
-    tasks_here_yet: "",
-    items_left: "",
-    clear_completed: "",
+    tasks_here_yet: "Задач пока нет.",
+    items_left: "0 оставшиеся задачи",
+    clear_completed: "Очистить завершеные",
     Russia_1: "Русский",
     English_1: "Англиский",
     Estonian_1: "Эстонский",
+    What_do_you_need: "Что вам нужно сделать?",
 
 },
 
@@ -34,12 +36,13 @@ et:{
     complete_1: "Lõpetatud",
     all_1: "Kõik",
     active_1: "Aktiivne",
-    tasks_here_yet: "",
-    items_left: "",
-    clear_completed: "",
+    tasks_here_yet: "Siin pole veel ülesandeid",
+    items_left: "0 allesjäänud esemeid",
+    clear_completed: "Tühjendamine lõpetatud",
     Russia_1: "Vene keel",
     English_1: "Inglise keel",
     Estonian_1: "Eesti keel",
+    What_do_you_need: "Mida sa pead tegema?",
 
 }
 };
